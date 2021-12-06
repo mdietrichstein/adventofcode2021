0ffd0a615540ccd19f265801f81bbef3a693e9d9
Marc Dietrichstein <mdietrichstein@users.noreply.github.com>
2021-12-06 16:23:13 +0000

    Prepare day5

---
+++ day05/src/main.rs
@@ -0,0 +1,32 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_DATA:&str = "
+        0,9 -> 5,9
+        8,0 -> 0,8
+        9,4 -> 3,4
+        2,2 -> 2,1
+        7,0 -> 7,4
+        6,4 -> 2,0
+        0,9 -> 2,9
+        3,4 -> 1,4
+        0,0 -> 8,8
+        5,5 -> 8,2
+    ";
+
+    #[test]
+    fn test_part1() {
+
+    }
+
+    #[test]
+    fn test_part2() {
+
+    }
+}
