b9dc251d352c54157b007d9cbedf04dc818f3a32
Marc Dietrichstein <marc@hero-workout.com>
2021-12-07 12:56:26 +0100

    Prepare day 8

---
+++ day08/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    const TEST_DATA: &str = "";
+
+    #[test]
+    fn test_part1() {
+
+    }
+
+    #[test]
+    fn test_part2() {
+
+    }
+}
