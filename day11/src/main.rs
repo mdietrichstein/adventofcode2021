79d24413decc1184b50179634046b794d8fb4501
Marc Dietrichstein <marc@hero-workout.com>
2021-12-11 15:56:18 +0100

    Prepare day 11

---
+++ day11/src/main.rs
@@ -0,0 +1,30 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+
+    const TEST_DATA: &str = "
+        5483143223
+        2745854711
+        5264556173
+        6141336146
+        6357385478
+        4167524645
+        2176841721
+        6882881134
+        4846848554
+        5283751526
+    ";
+
+    #[test]
+    fn test_part1() {
+
+    }
+
+    #[test]
+    fn test_part2() {
+
+    }
+}
