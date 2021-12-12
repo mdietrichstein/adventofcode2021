ba0138d32c3ec24585544ca84b27cbc41f7c81cb
Marc Dietrichstein <marc@hero-workout.com>
2021-12-12 10:48:11 +0100

    Solve day 11 part 1

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,9 +1,64 @@
+use map::Map;
+
+mod map;
+
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../resources/input");
+
+    let num_flashes = count_flashes(input);
+    println!("[1/2] Result: {}", num_flashes);
+}
+
+fn count_flashes(input: &str) -> usize {
+    let mut map = Map::new(input);
+    let mut positions_to_check: Vec<usize> = vec![];
+
+    let mut num_flashes: usize = 0;
+
+    // println!("Before any steps:\n-------------------\n{}\n-------------------\n\n", map);
+
+    let num_steps = 100;
+    for step in 0..num_steps {
+        for i in 0..map.len() {
+            map[i] += 1;
+
+            if map[i] == 10 {
+                positions_to_check.push(i);
+            }
+        }
+
+        while let Some(position) = positions_to_check.pop() {
+            let neighborhood_indices = map.neighborhood_positions(position);
+
+            if let Some(neighborhood_indices) = neighborhood_indices {
+                for index in neighborhood_indices {
+                    if let Some(index) = index {
+                        map[index] += 1;
+
+                        if map[index] == 10 {
+                            positions_to_check.push(index);
+                        }
+                    }
+                }
+            }
+        }
+
+        for i in 0..map.len() {
+            if map[i] >= 10 {
+                num_flashes += 1;
+                map[i] = 0;
+            }
+        }
+
+        // println!("After step {}:\n-------------------\n{}\n-------------------\n\n", step + 1, map);
+    }
+
+    num_flashes
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
 
     const TEST_DATA: &str = "
         5483143223
@@ -20,11 +75,10 @@ mod tests {
 
     #[test]
     fn test_part1() {
-
+        let num_flashes = count_flashes(TEST_DATA);
+        assert_eq!(1656, num_flashes);
     }
 
     #[test]
-    fn test_part2() {
-
-    }
+    fn test_part2() {}
 }
