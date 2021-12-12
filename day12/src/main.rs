859c576a0c1d30283edee27b2db9d759b9629c1a
Marc Dietrichstein <marc@hero-workout.com>
2021-12-12 19:36:08 +0100

    Solve day 12 part 1

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,16 +1,68 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+
+const START_NODE: &str = "start";
+const END_NODE: &str = "end";
+
 fn main() {
     let input = include_str!("../resources/input");
-    let num_paths = count_paths(input);
-    println!("[1/2] Result: {}", num_paths);
+    let paths = find_paths(input);
+    println!("[1/2] Result: {}", paths.len());
 }
 
-fn count_paths(input: &str) -> usize {
-    todo!()
+fn is_lowercase(value: &str) -> bool {
+    value.to_lowercase() == value
 }
+
+fn visit_node<'a>(node: &'a str, mut visited: Vec<&'a str>, connections: &HashMap<&'a str, HashSet<&'a str>>, paths: &mut Vec<Vec<&'a str>>) {
+    visited.push(node);
+
+    for child in &connections[node] {
+        if is_lowercase(child) && visited.contains(&child) {
+            continue;
+        }
+
+        if *child == END_NODE {
+            let mut path = visited.clone();
+            path.push(child);
+            paths.push(path);
+            continue;
+        }
+
+        visit_node(child, visited.clone(), connections, paths);
+    }
+}
+
+fn find_paths(input: &str) -> Vec<Vec<&str>> {
+    let mut connections: HashMap<&str, HashSet<&str>> = HashMap::new();
+
+    input.trim().split('\n')
+        .map(|line| line.trim().split_once('-').unwrap())
+        .for_each(|(a, b)| {
+            let nodes = connections.entry(a).or_default();
+            nodes.insert(b);
+
+            let nodes = connections.entry(b).or_default();
+
+            if a != START_NODE && b != END_NODE {
+                nodes.insert(a);
+            }
+        });
+
+    let mut paths: Vec<Vec<&str>> = vec![];
+
+    let mut visited: Vec<&str> = vec![];
+    visit_node(START_NODE, visited, &connections, &mut paths);
+
+    paths
+}
+
 
 #[cfg(test)]
 mod tests {
-    use crate::count_paths;
+    use std::collections::{HashMap, HashSet};
+
+    use crate::find_paths;
 
     const TEST_DATA_SMALL: &str = "
         start-A
@@ -58,9 +110,10 @@ mod tests {
 
     #[test]
     pub fn test_part1() {
-        assert_eq!(10, count_paths(TEST_DATA_SMALL));
-        assert_eq!(19, count_paths(TEST_DATA_MEDIUM));
-        assert_eq!(226, count_paths(TEST_DATA_LARGE));
+        find_paths(TEST_DATA_MEDIUM);
+        assert_eq!(10, find_paths(TEST_DATA_SMALL).len());
+        assert_eq!(19, find_paths(TEST_DATA_MEDIUM).len());
+        assert_eq!(226, find_paths(TEST_DATA_LARGE).len());
     }
 
     #[test]
