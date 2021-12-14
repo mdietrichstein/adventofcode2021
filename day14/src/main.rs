3907fbccedb15a1a968c96febb769d050de63fa6
Marc Dietrichstein <marc@hero-workout.com>
2021-12-14 14:09:55 +0100

    Prepare day 14

---
+++ day14/src/main.rs
@@ -0,0 +1,33 @@
+fn main() {
+    let input = include_str!("../resources/input");
+}
+
+#[cfg(test)]
+mod tests {
+    const TEST_DATA: &str = "
+        NNCB
+
+        CH -> B
+        HH -> N
+        CB -> H
+        NH -> C
+        HB -> C
+        HC -> B
+        HN -> C
+        NN -> C
+        BH -> H
+        NC -> B
+        NB -> B
+        BN -> B
+        BB -> N
+        BC -> B
+        CC -> N
+        CN -> C
+    ";
+
+    #[test]
+    fn test_part1() {}
+
+    #[test]
+    fn test_part2() {}
+}
