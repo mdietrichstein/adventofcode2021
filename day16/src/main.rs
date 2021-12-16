be2267d06f932b7612ca03b87dde8f70ed4dafcc
Marc Dietrichstein <marc@hero-workout.com>
2021-12-16 11:57:46 +0100

    Prepare day 16

---
+++ day16/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let input = include_str!("../resources/input");
+    println!("[1/2] Result: {}", None);
+}
+
+#[cfg(test)]
+mod tests {
+    const TEST_DATA_ID_0: &str = "38006F45291200";
+    const TEST_DATA_ID_1: &str = "EE00D40C823060";
+
+    #[test]
+    pub fn test_part1() {}
+
+    #[test]
+    pub fn test_part2() {}
+}
