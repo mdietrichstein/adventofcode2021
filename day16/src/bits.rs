8097a71b6e79af5609ea82326285d214a10f8944
Marc Dietrichstein <marc@hero-workout.com>
2021-12-17 17:15:30 +0100

    Solve day 16 part 1

---
+++ day16/src/bits.rs
@@ -0,0 +1,127 @@
+pub struct BitReader {
+    bytes: Vec<u8>,
+    byte_offset: usize,
+    remaining_bits: usize,
+    current_byte: u8,
+}
+
+impl BitReader {
+    pub fn new(input: &str) -> Self {
+        let bytes = str_to_bytes(input);
+        let first_byte = bytes[0];
+
+        BitReader {
+            bytes,
+            byte_offset: 0,
+            remaining_bits: 8,
+            current_byte: first_byte,
+        }
+    }
+
+    pub fn read(&mut self, nbits: usize) -> Option<u8> {
+        if nbits > 8 {
+            panic!("can read 8 bits max")
+        }
+
+        if self.byte_offset >= self.bytes.len() {
+            return None;
+        }
+
+        if nbits <= self.remaining_bits {
+            let result = self.read_from_current_byte(nbits);
+            self.remaining_bits -= nbits;
+
+            if self.remaining_bits != 0 {
+                self.current_byte = self.current_byte << nbits;
+            }
+
+            self.update_state();
+
+            return Some(result);
+        } else {
+            let remainder_bits = nbits - self.remaining_bits;
+
+            let upper = self.read_from_current_byte(self.remaining_bits);
+            self.remaining_bits = 0;
+            self.update_state();
+
+            let lower = self.read_from_current_byte(remainder_bits);
+            self.remaining_bits = 8 - remainder_bits;
+
+            let result = (upper << remainder_bits) | lower;
+
+            self.current_byte = self.current_byte << remainder_bits;
+
+            return Some(result);
+        }
+    }
+
+    pub fn read_u64(&mut self, nbits: usize) -> Option<u64> {
+        let nbytes = (nbits / 8) + 1;
+        let trailing_bits = nbits % 8;
+
+        let mut result: u64 = 0;
+
+        for i in 0..nbytes {
+            let bits = if i == nbytes - 1 {
+                trailing_bits
+            } else {
+                8
+            };
+
+            let value = self.read(bits)? as u64;
+
+            let byte_index = nbytes - 1 - i;
+
+            if byte_index == 0 {
+                result = result | value;
+            } else {
+                result = result | (value << (trailing_bits + (8 * (byte_index - 1))));
+            }
+        }
+
+
+        Some(result)
+    }
+
+    fn read_from_current_byte(&self, nbits: usize) -> u8 {
+        if nbits == 8 {
+            self.current_byte
+        } else {
+            ((self.current_byte >> (8 - nbits))) & (2_u8.pow(nbits as u32) - 1)
+        }
+    }
+
+    fn update_state(&mut self) {
+        if self.remaining_bits == 0 {
+            self.remaining_bits = 8;
+            self.byte_offset += 1;
+            self.current_byte = self.bytes[self.byte_offset];
+        }
+    }
+}
+
+pub fn print_binary(input: &str) {
+    let bytes = str_to_bytes(input);
+
+    for b in bytes {
+        print!("{:b}", b);
+    }
+
+    println!();
+}
+
+fn str_to_bytes(input: &str) -> Vec<u8> {
+    let input = if input.len() % 2 == 0 {
+        input.to_string()
+    } else {
+        format!("{}0", input)
+    };
+
+    let bytes = input.chars().collect::<Vec<char>>()
+        .chunks(2).into_iter()
+        .map(|hex| format!("{}{}", hex[0], hex[1]))
+        .map(|byte| u8::from_str_radix(&byte, 16).unwrap()).collect::<Vec<u8>>();
+
+    bytes
+}
--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,16 +1,146 @@
+use bits::BitReader;
+
+use crate::Packet::LiteralPacket;
+
+mod bits;
+
 fn main() {
-    let input = include_str!("../resources/input");
-    println!("[1/2] Result: {}", None);
+    let input = include_str!("../resources/input").trim();
+    println!("[1/2] Result: {}", sum_version_numbers_for_input(input));
+}
+
+#[derive(Debug)]
+enum Packet {
+    LiteralPacket {
+        version: u8,
+        value: u64,
+    },
+    OperatorPacket {
+        version: u8,
+        packets: Vec<Packet>,
+    },
+}
+
+fn parse_literal_packet(version: u8, bit_reader: &mut BitReader) -> Option<(usize, Packet)> {
+    let mut nibbles: Vec<u8> = vec![];
+
+    loop {
+        let group = bit_reader.read(5)?;
+        nibbles.push(group & 0b00001111);
+
+        if (group & 0b00010000) == 0 {
+            break;
+        }
+    }
+
+    let num_nibbles = nibbles.len();
+
+    let mut value: u64 = 0;
+    for i in 0..num_nibbles {
+        value = value | (nibbles[i] as u64) << ((num_nibbles - 1) - i) * 4;
+    }
+
+    let bits_read = num_nibbles * 5;
+
+    Some((
+        bits_read,
+        LiteralPacket {
+            version,
+            value,
+        }
+    ))
+}
+
+fn parse_operator_packet(version: u8, bit_reader: &mut BitReader) -> Option<(usize, Packet)> {
+    let indicator = bit_reader.read(1)?;
+
+    let mut bits_read: usize = 1;
+    let mut packets: Vec<Packet> = vec![];
+
+    if indicator == 0 {
+        bits_read += 15;
+
+        let length_in_bits = bit_reader.read_u64(15)? as usize;
+
+        let mut nested_bits_read = 0;
+
+        loop {
+            let (nbits, packet) = parse_packet(bit_reader)?;
+            packets.push(packet);
+            nested_bits_read += nbits;
+
+            if nested_bits_read >= length_in_bits {
+                break;
+            }
+        }
+
+        bits_read += nested_bits_read;
+    } else {
+        let length_in_packets = bit_reader.read_u64(11)?;
+        bits_read += 11;
+
+        let mut nested_bits_read = 0;
+
+        for _ in 0..length_in_packets {
+            let (nbits, packet) = parse_packet(bit_reader)?;
+            packets.push(packet);
+            nested_bits_read += nbits;
+        }
+
+        bits_read += nested_bits_read;
+    }
+
+    Some((
+        bits_read,
+        Packet::OperatorPacket {
+            version,
+            packets,
+        }
+    ))
+}
+
+
+fn parse_packet(bit_reader: &mut BitReader) -> Option<(usize, Packet)> {
+    let version = bit_reader.read(3).unwrap();
+    let packet_type = bit_reader.read(3).unwrap();
+
+    if let Some((bits_read, packet)) = match packet_type {
+        4 => parse_literal_packet(version, bit_reader),
+        _ => parse_operator_packet(version, bit_reader),
+    } {
+        Some((6 + bits_read, packet))
+    } else {
+        None
+    }
+}
+
+fn sum_version_numbers(packet: &Packet) -> usize {
+    match packet {
+        Packet::LiteralPacket { version, value } => *version as usize,
+        Packet::OperatorPacket { version, packets } => (*version as usize) + packets.iter().map(sum_version_numbers).sum::<usize>(),
+    }
+}
+
+fn sum_version_numbers_for_input(input: &str) -> usize {
+    let mut bit_reader = BitReader::new(input);
+    let packet = parse_packet(&mut bit_reader).unwrap().1;
+    sum_version_numbers(&packet)
 }
 
 #[cfg(test)]
 mod tests {
-    const TEST_DATA_ID_0: &str = "38006F45291200";
-    const TEST_DATA_ID_1: &str = "EE00D40C823060";
+    use super::*;
 
     #[test]
-    pub fn test_part1() {}
+    pub fn test_part1() {
+        assert_eq!(16, sum_version_numbers_for_input("8A004A801A8002F478"));
+        assert_eq!(12, sum_version_numbers_for_input("620080001611562C8802118E34"));
+        assert_eq!(23, sum_version_numbers_for_input("C0015000016115A2E0802F182340"));
+        assert_eq!(31, sum_version_numbers_for_input("A0016C880162017C3686B18A3D4780"));
+    }
 
     #[test]
-    pub fn test_part2() {}
+    pub fn test_part2() {
+
+    }
 }
