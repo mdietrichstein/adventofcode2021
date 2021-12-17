15a8a1ddf0b16f6dbd268bc9a46d34186b33184e
Marc Dietrichstein <marc@hero-workout.com>
2021-12-17 18:36:49 +0100

    Prepare day 17

---
+++ day17/src/main.rs
@@ -0,0 +1,18 @@
+use map::Map;
+
+mod map;
+
+fn main() {
+    let input = include_str!("../resources/input");
+}
+
+#[cfg(test)]
+mod tests {
+    const TEST_DATA: &str = "target area: x=20..30, y=-10..-5";
+
+    #[test]
+    fn test_part1() {}
+
+    #[test]
+    fn test_part2() {}
+}
