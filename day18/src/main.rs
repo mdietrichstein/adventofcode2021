23a7aa1137c980ddc87362d18803cd05f1d8f990
Marc Dietrichstein <marc@hero-workout.com>
2021-12-18 19:16:18 +0100

    Prepare day 18

---
+++ day18/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let input = include_str!("../resources/input");
+    // println!("[1/2] Result: {}", 0);
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_part1() {
+        
+    }
+
+    #[test]
+    fn test_part2() {
+
+    }
+}
